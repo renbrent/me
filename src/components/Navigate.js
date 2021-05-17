c7209d115ef0c13ef6ae36a9a513074b5c8ffb99
armallama <58412994+armallama@users.noreply.github.com>
2021-05-17 13:55:35 +0900

    Fix anchor warnings

--- src/components/Navigate.js
+++ src/components/Navigate.js
@@ -1,4 +1,13 @@
 const Navigate = () => {
+
+    const anchorHide = {
+        height: "1px",
+        width: "1px",
+        position: "absolute",
+        overflow: "hidden",
+        top: "-10px",
+    }
+
     return (
         <nav className="uk-navbar-container uk-margin uk-navbar" uk-navbar data-uk-sticky>
             <div className="uk-navbar-left">
@@ -20,11 +29,11 @@ const Navigate = () => {
             </div>
             <div className="uk-navbar-right">
                 <ul className="uk-navbar-nav uk-visible@s">
-                    <li><a href="https://github.com/renbrent" target="_blank" rel="noopener noreferrer" className="uk-icon-hover" uk-icon="github"></a></li>  
-                    <li><a href="https://www.linkedin.com/in/brentbasiano/" target="_blank" rel="noopener noreferrer" className="uk-icon-hover" uk-icon="linkedin"></a></li>
-                    <li><a href="mailto:brentarnold.basiano@gmail.com" className="uk-icon-hover" uk-icon="mail"></a></li>                  
+                    <li><a href="https://github.com/renbrent" target="_blank" rel="noopener noreferrer" className="uk-icon-hover" uk-icon="github"><span style={anchorHide}></span></a></li>  
+                    <li><a href="https://www.linkedin.com/in/brentbasiano/" target="_blank" rel="noopener noreferrer" className="uk-icon-hover" uk-icon="linkedin"><span style={anchorHide}></span></a></li>
+                    <li><a href="mailto:brentarnold.basiano@gmail.com" className="uk-icon-hover" uk-icon="mail"><span style={anchorHide}></span></a></li>                  
                 </ul>
-                <a href="#offcanvas" className="uk-navbar-toggle uk-hidden@s" uk-icon="menu" uk-toggle="target: #offcanvas"></a>
+                <a href="#offcanvas" className="uk-navbar-toggle uk-hidden@s" uk-icon="menu" uk-toggle="target: #offcanvas"><span style={anchorHide}></span></a>
             </div>
             <div id="offcanvas" uk-offcanvas="flip: true; overlay: false">
                 <div class="uk-offcanvas-bar">
@@ -37,9 +46,9 @@ const Navigate = () => {
                         <li><a href="#about">About</a></li>
                         <li><a href="#projects">Projects</a></li>
                         <li class="uk-nav-header"><h4>Connect</h4></li>
-                        <li><a href="https://github.com/renbrent" target="_blank" rel="noopener noreferrer" className="uk-icon-hover" uk-icon="github"></a></li>  
-                        <li><a href="https://www.linkedin.com/in/brentbasiano/"  target="_blank" rel="noopener noreferrer"className="uk-icon-hover" uk-icon="linkedin"></a></li>
-                        <li><a href="mailto:brentarnold.basiano@gmail.com" className="uk-icon-hover" uk-icon="mail"></a></li>   
+                        <li><a href="https://github.com/renbrent" target="_blank" rel="noopener noreferrer" className="uk-icon-hover" uk-icon="github"><span style={anchorHide}></span></a></li>  
+                        <li><a href="https://www.linkedin.com/in/brentbasiano/"  target="_blank" rel="noopener noreferrer"className="uk-icon-hover" uk-icon="linkedin"><span style={anchorHide}></span></a></li>
+                        <li><a href="mailto:brentarnold.basiano@gmail.com" className="uk-icon-hover" uk-icon="mail"><span style={anchorHide}></span></a></li>   
                     </ul>
                 </div>
             </div>
